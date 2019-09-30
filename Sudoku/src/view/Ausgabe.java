d87b386a8e6306a1930a423cfe3629de2b221efc
philipp.koren <philipp.koren@r117-10.ad.b-cw-hwos.logodidact.net>
2019-09-30 13:21:55 +0200

    Ausgabe Klasse & Funktion erstellt.

--- Sudoku/src/view/Ausgabe.java
+++ Sudoku/src/view/Ausgabe.java
@@ -4,15 +4,46 @@
  * and open the template in the editor.
  */
 package view;
-
+import model.Sudoku;
 /**
  *
  * @author yannis.hofmann
  */
-public class Ausgabe {
+public class Ausgabe{
     
-    public String sudokuAusgeben(){
-        
-        return null;
+    public void sudokuAusgeben(Sudoku sdk){
+        if(!sdk.geloest()){  //Überprüfung ob das Sudoku gelöst ist.
+            System.out.println("Das Sudoku wurde noch nicht gelöst.");
+            
+            short[][] werteNetz = sdk.getWerteNetz();
+            short size = sdk.getSize();
+            
+            for(short x=0;x<size;x++){
+                String str = "";
+                for(short y=0;y<size;y++){
+                    if(werteNetz[x][y] == 0) str += "[ ]";
+                    else str += "["+werteNetz[x][y]+"]";
+                }
+                System.out.println(str);  //Ausgabe des ungelösten Sudoku. 
+            }
+        }
+        else if(sdk.loesbar()){    //Lösungsalgorithmus endet bei "Schritt 3".
+            System.out.println("Das Sudoku wurde erfolgreich gelöst. Das Fertige Sudoku:");
+            
+            short[][] werteNetz = sdk.getWerteNetz();
+            short size = sdk.getSize();
+            
+            for(short x=0;x<size;x++){
+                String str = "";
+                for(short y=0;y<size;y++){
+                    str += "["+werteNetz[x][y]+"]";
+                }
+                System.out.println(str); //Ausgabe des gelösten Sudokus.
+            }
+            
+        }
+        else{
+            System.out.println("Das Sudoku ist nicht lösbar");
+        }
     }
 }
